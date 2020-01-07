e7bb78f32f7873f5dbb30eb911008f9e3bb156f3
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-07 12:41:55 +0800

    Add a navigation bar at the top of all webpages, links to about page and contact page which are currently empty

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });  
 
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'static', 'about.html'));  
+});
+
+app.get('/contact', (req,res) => {
+    res.sendFile(path.join(__dirname, 'static', 'contact.html'));  
+});
+
 app.post('/notes', (req,res) => {
     if (req.body.topic == 1) {
         res.sendFile(path.join(__dirname, 'static', 'first.html'));      
